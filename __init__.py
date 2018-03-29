ab2f90aabb1ae3fcfe1ce90b57ba83a0ac687a3c
otapi <31888571+otapi@users.noreply.github.com>
2018-03-29 18:58:28 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,208 @@
+﻿#!/usr/bin/env python
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+from __future__ import (unicode_literals, division, absolute_import,
+						print_function)
+
+__license__   = 'GPL v3'
+__copyright__ = '2011-2018, Hoffer Csaba <csaba.hoffer@gmail.com>, Kloon <kloon@techgeek.co.in>, otapi <otapigems.com>'
+__docformat__ = 'restructuredtext hu'
+
+import time
+import urllib
+from Queue import Queue, Empty
+from lxml.html import fromstring
+import lxml.etree as etree
+from calibre import as_unicode
+from calibre.ebooks.metadata import check_isbn
+from calibre.ebooks.metadata.sources.base import Source, Option
+from calibre.utils.cleantext import clean_ascii_chars
+import lxml, sys, traceback
+from calibre import browser
+
+class Moly_hu(Source):
+	name					= 'Moly_hu'
+	description				= _('Downloads metadata and covers from moly.hu')
+	author					= 'Hoffer Csaba & Kloon & fatsadt & otapi'
+	version					= (1, 0, 5)
+	minimum_calibre_version = (0, 8, 0)
+
+	capabilities = frozenset(['identify', 'cover'])
+	touched_fields = frozenset(['title', 'authors', 'identifier:isbn', 'identifier:moly_hu', 'tags', 'comments', 'rating', 'series', 'series_index', 'publisher', 'pubdate', 'language', 'languages'])
+	has_html_comments = False
+	supports_gzip_transfer_encoding = False
+	can_get_multiple_covers = True
+
+	KEY_MAX_BOOKS = 'max_books'
+	KEY_MAX_COVERS = 'max_covers'
+
+	options = (Option(KEY_MAX_BOOKS, 'number', 3, _('Maximum number of books to get'),
+                      _('The maximum number of books to process from the moly.hu search result')),
+				Option(KEY_MAX_COVERS, 'number', 5, _('Maximum number of covers to get'),
+                      _('The maximum number of covers to process for the chosen book'))
+	)
+
+	BASE_URL = 'https://moly.hu'
+	BOOK_URL = BASE_URL + '/konyvek/'
+	SEARCH_URL = BASE_URL + '/kereses?utf8=%E2%9C%93&q='
+
+	def create_query(self, log, title=None, authors=None, identifiers={}):
+		if title is not None:
+			search_title = urllib.quote(title.encode('utf-8'))
+		else:
+			search_title = ''
+
+		if authors is not None:
+			search_author = urllib.quote(authors[0].encode('utf-8'))
+		else:
+			search_author = ''
+
+		search_page = Moly_hu.SEARCH_URL + '%s+%s'%(search_author, search_title)
+
+		return search_page
+	def get_cached_cover_url(self, identifiers):
+		url = None
+		moly_id = identifiers.get('moly_hu', None)
+		if moly_id is None:
+			isbn = check_isbn(identifiers.get('isbn', None))
+			if isbn is not None:
+				moly_id = self.cached_isbn_to_identifier(isbn)
+		if moly_id is not None:
+			url = self.cached_identifier_to_cover_url(moly_id)
+		return url
+
+	def identify(self, log, result_queue, abort, title, authors,
+			identifiers={}, timeout=30):
+		'''
+		Note this method will retry without identifiers automatically if no
+		match is found with identifiers.
+		'''
+		matches = []
+		moly_id = identifiers.get('moly_hu', None)
+		log.info(u'\nTitle:%s\nAuthors:%s\n'%(title, authors))
+		br = browser()
+		if moly_id:
+			matches.append(Moly_hu.BOOK_URL + moly_id)
+		else:
+			query = self.create_query(log, title=title, authors=authors, identifiers=identifiers)
+			if query is None:
+				log.error('Insufficient metadata to construct query')
+				return
+			try:
+				log.info('Querying: %s'%query)
+				response = br.open(query)
+			except Exception as e:
+				if callable(getattr(e, 'getcode', None)) and e.getcode() == 404:
+					log.info('Failed to find match for ISBN: %s'%isbn)
+				else:
+					err = 'Failed to make identify query: %r'%query
+					log.exception(err)
+					return as_unicode(e)
+
+			try:
+				raw = response.read().strip()
+				raw = raw.decode('utf-8', errors='replace')
+				if not raw:
+					log.error('Failed to get raw result for query: %r'%query)
+					return
+				root = fromstring(clean_ascii_chars(raw))
+			except:
+				msg = 'Failed to parse moly.hu page for query: %r'%query
+				log.exception(msg)
+				return msg
+			self._parse_search_results(log, title, authors, root, matches, timeout)
+
+		if abort.is_set():
+			return
+
+		if not matches:
+			if identifiers and title and authors:
+				log.info('No matches found with identifiers, retrying using only'
+						' title and authors')
+				return self.identify(log, result_queue, abort, title=title,
+						authors=authors, timeout=timeout)
+			log.error('No matches found with query: %r'%query)
+			return
+
+		from calibre_plugins.moly_hu.worker import Worker
+		workers = [Worker(url, result_queue, br, log, i, self) for i, url in
+				enumerate(matches)]
+
+		for w in workers:
+			w.start()
+			time.sleep(0.1)
+		
+		
+		while not abort.is_set():
+			a_worker_is_alive = False
+			for w in workers:
+				w.join(0.2)
+				if abort.is_set():
+					break
+				if w.is_alive():
+					a_worker_is_alive = True
+			if not a_worker_is_alive:
+				break
+
+		return None
+	def _parse_search_results(self, log, orig_title, orig_authors, root, matches, timeout):
+		max_results = self.prefs[Moly_hu.KEY_MAX_BOOKS]
+		results = root.xpath('//a[@class="book_selector"]')
+		log.info('Found %d possible books (max: %d)'%(len(results), max_results))
+		i = 0
+		for result in results:
+			book_urls = result.xpath('@href')
+			etree.strip_tags(result, 'strong')
+			author_n_title = result.text
+			author_n_titles = author_n_title.split(':', 1)
+			author = author_n_titles[0].strip(' \r\n\t')
+			title = author_n_titles[1].strip(' \r\n\t')
+			
+			if orig_title:
+				if orig_title.lower() not in title.lower():
+					continue
+			if orig_authors:
+				if orig_authors[0].lower() not in author.lower():
+					continue
+			
+			for book_url in book_urls:
+				result_url = Moly_hu.BASE_URL + book_url
+				
+				if (result_url not in matches):
+					matches.append(result_url)
+					i += 1
+				if (i >= max_results):
+					return
+					
+	def download_cover(self, log, result_queue, abort,
+            title=None, authors=None, identifiers={}, timeout=30, get_best_cover=False):
+        if not title:
+            return
+        urls = self.get_image_urls(title, authors, identifiers, log, abort, timeout)
+        self.download_multiple_covers(title, authors, urls, get_best_cover, timeout, result_queue, abort, log)
+
+	def get_image_urls(self, title, authors, identifiers, log, abort, timeout):
+        cached_url = self.get_cached_cover_url(identifiers)
+		if cached_url is None:
+			log.info('No cached cover found, running identify')
+			rq = Queue()
+			self.identify(log, rq, abort, title=title, authors=authors, identifiers=identifiers)
+			if abort.is_set():
+				return
+			results = []
+			while True:
+				try:
+					results.append(rq.get_nowait())
+				except Empty:
+					break
+			results.sort(key=self.identify_results_keygen(
+				title=title, authors=authors, identifiers=identifiers))
+			for mi in results:
+				cached_url = self.get_cached_cover_url(mi.identifiers)
+				if cached_url is not None:
+					break
+
+		if cached_url is not None:
+			return cached_url
+
+		log.info('No cover found')
+		return []
---
+++ worker.py
@@ -0,0 +1,282 @@
+﻿#!/usr/bin/env python
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+from __future__ import (unicode_literals, division, absolute_import,
+						print_function)
+
+__license__   = 'GPL v3'
+__copyright__ = '2011-2014, Hoffer Csaba <csaba.hoffer@gmail.com>, Kloon <kloon@techgeek.co.in>'
+__docformat__ = 'restructuredtext hu'
+
+import socket, re, datetime
+from threading import Thread
+from lxml.html import fromstring
+from calibre.ebooks.metadata.book.base import Metadata
+import lxml, sys
+import lxml.html as lh
+from calibre.utils.date import utcnow
+from datetime import datetime
+from dateutil import parser
+from calibre.ebooks.metadata import MetaInformation
+from calibre.utils.cleantext import clean_ascii_chars
+from calibre import browser
+
+class Worker(Thread): # Get details
+	'''
+	Get book details from moly.hu book page in a separate thread
+	'''
+
+	def __init__(self, url, result_queue, browser, log, relevance, plugin, timeout=30):
+		Thread.__init__(self)
+		self.daemon = True
+		self.url, self.result_queue = url, result_queue
+		self.log, self.timeout = log, timeout
+		self.relevance, self.plugin = relevance, plugin
+		self.browser = browser.clone_browser()
+		self.max_covers = plugin.prefs[plugin.KEY_MAX_COVERS]
+		self.cover_url = self.moly_id = self.isbn = None
+
+	def run(self):
+		try:
+			self.get_details()
+		except:
+			self.log.exception('get_details failed for url: %r'%self.url)
+
+	def get_details(self):
+		try:
+			raw = self.browser.open_novisit(self.url, timeout=self.timeout).read().strip()
+			raw = raw.decode('utf-8', errors='replace')
+			if not raw:
+				log.error('Failed to get raw result for query: %r'%query)
+				return
+		except Exception as e:
+			if callable(getattr(e, 'getcode', None)) and e.getcode() == 404:
+				self.log.error('URL malformed: %r'%self.url)
+				return
+			attr = getattr(e, 'args', [None])
+			attr = attr if attr else [None]
+			if isinstance(attr[0], socket.timeout):
+				msg = 'Moly.hu timed out. Try again later.'
+				self.log.error(msg)
+			else:
+				msg = 'Failed to make details query: %r'%self.url
+				self.log.exception(msg)
+			return
+
+		root = fromstring(clean_ascii_chars(raw))
+		self.parse_details(root)
+
+	def parse_details(self, root):
+		try:
+			moly_id = self.parse_moly_id(self.url)
+			self.log.info('Parsed moly.hu identifier: %s'%moly_id)
+		except:
+			self.log.exception('Error parsing moly.hu id for url: %r'%self.url)
+			moly_id = None
+
+		try:
+			title = self.parse_title(root)
+			self.log.info('Parsed title: %s'%title)
+		except:
+			self.log.exception('Error parsing title for url: %r'%self.url)
+			title = None
+		
+		try:
+			authors = self.parse_authors(root)
+			self.log.info('Parsed authors: %s'%authors)
+		except:
+			self.log.exception('Error parsing authors for url: %r'%self.url)
+			authors = []
+
+		if not title or not authors or not moly_id:
+			self.log.error('Could not find title/authors/moly.hu id for %r'%self.url)
+			self.log.error('Moly.hu id: %r Title: %r Authors: %r'%(moly_id, title, authors))
+			return
+
+		mi = Metadata(title, authors)
+		mi.set_identifier('moly_hu', moly_id)
+		self.moly_id = moly_id
+
+		try:
+			isbn = self.parse_isbn(root)
+			self.log.info('Parsed ISBN: %s'%isbn)
+			if isbn:
+				self.isbn = mi.isbn = isbn
+		except:
+			self.log.exception('Error parsing ISBN for url: %r'%self.url)
+		
+		try:
+			series_info = self.parse_series(root)
+			if series_info is not None:
+				mi.series = series_info[0]
+				mi.series_index = int(series_info[1])
+				self.log.info('Parsed series: %s, series index: %f'%(mi.series,mi.series_index))
+		except:
+			self.log.exception('Error parsing series for url: %r'%self.url)
+			
+		try:
+			mi.comments = self.parse_comments(root)
+			self.log.info('Parsed comments: %s'%mi.comments)
+		except:
+			self.log.exception('Error parsing comments for url: %r'%self.url)
+
+		try:
+			self.cover_url = self.parse_covers(root)
+			self.log.info('Parsed URL for cover: %r'%self.cover_url)
+			self.plugin.cache_identifier_to_cover_url(self.moly_id, self.cover_url)
+			mi.has_cover = bool(self.cover_url)
+		except:
+			self.log.exception('Error parsing cover for url: %r'%self.url)
+
+		try:
+			mi.tags = self.parse_tags(root)
+			self.log.info('Parsed tags: %s'%mi.tags)
+		except:
+			self.log.exception('Error parsing tags for url: %r'%self.url)
+			
+		try:
+			mi.languages = self.parse_languages(mi.tags)
+			self.log.info('Parsed languages: %r'%mi.languages)
+		except:
+			self.log.exception('Error parsing language for url: %r'%self.url)
+			
+		try:
+			mi.publisher = self.parse_publisher(root)
+			self.log.info('Parsed publisher: %s'%mi.publisher)
+		except:
+			self.log.exception('Error parsing publisher for url: %r'%self.url)	
+			
+		try:
+			mi.pubdate = self.parse_published_date(root)
+			self.log.info('Parsed publication date: %s'%mi.pubdate)
+		except:
+			self.log.exception('Error parsing published date for url: %r'%self.url)
+			
+		try:
+			mi.rating = self.parse_rating(root)
+			self.log.info('Parsed rating: %s\n\n'%mi.rating)
+		except:
+			self.log.exception('Error parsing tags for url: %r\n\n'%self.url)
+
+
+		mi.source_relevance = self.relevance
+
+		if self.moly_id and self.isbn:
+			self.plugin.cache_isbn_to_identifier(self.isbn, self.moly_id)
+
+		self.plugin.clean_downloaded_metadata(mi)
+
+		self.result_queue.put(mi)
+
+	def parse_moly_id(self, url):
+		try:
+			m = re.search('/konyvek/(.*)', url)
+			if m:
+				return m.group(1)
+		except:
+			return None
+		
+	def parse_isbn(self, root):
+		isbn = None
+		isbn_node = root.xpath('//*[@id="content"]//*[@class="items"]/div/div[2]/text()')
+		for isbn_value in isbn_node:
+			m = re.search('(\d{13}|\d{10})', isbn_value)
+			if m:
+				isbn = m.group(1)
+				break
+		
+		return isbn
+		
+	def parse_title(self, root):
+		title_node = root.xpath('//*[@id="content"]//*[@class="fn"]/text()')
+		self.log.info('Title: %s'%title_node)
+		if title_node:
+			return title_node[0].strip()
+			
+	def parse_series(self, root):
+		series_node = root.xpath('//*[@id="content"]//*[@class="fn"]/a/text()')
+		if not series_node:
+			return None
+		
+		return series_node[0].strip('().').rsplit(' ', 1)
+		
+	def parse_authors(self, root):
+		author_nodes = root.xpath('//*[@id="content"]//div[@class="authors"]/a/text()')
+		self.log.info('Authors: %r'%author_nodes)
+		if author_nodes:
+			return [unicode(author) for author in author_nodes]
+
+	def parse_tags(self, root):
+		tags_node = root.xpath('//*[@id="tags"]//*[@class="hover_link"]/text()')
+		tags_node = [unicode(text) for text in tags_node if text.strip()]
+		if tags_node:
+			return tags_node
+			
+	def parse_comments(self, root):
+		description_node = root.xpath('//*[@id="content"]//*[@class="text" and @id="full_description"]/p/text()')
+		if not description_node:
+			description_node = root.xpath('//*[@id="content"]//*[@class="text"]/p/text()')
+		
+		if description_node:
+			return '\n'.join(description_node)
+			
+	def parse_publisher(self, root):
+		publisher_node = root.xpath('//*[@id="content"]//*[@class="items"]/div/div[1]/a/text()')
+		if publisher_node:
+			return publisher_node[0]
+		
+	def parse_published_date(self, root):
+		pub_year = None
+		publication_node = root.xpath('//*[@id="content"]//*[@class="items"]/div/div[1]/text()')
+		for publication_value in publication_node:
+			m = re.search('(\d{4})', publication_value)
+			if m:
+				pub_year = m.group(1)
+				break
+		
+		if not pub_year:
+			return None
+		default = datetime.utcnow()
+		from calibre.utils.date import utc_tz
+		default = datetime(default.year, default.month, default.day, tzinfo=utc_tz)
+		pub_date = parser.parse(pub_year, default=default)
+		if pub_date:
+			return pub_date
+	
+	def parse_rating(self, root):
+		rating_node = root.xpath('//*[@id="content"]//*[@class="rating"]//*[@class="like_count"]/text()')
+		#rating_node = round(float(rating_node[0].strip().strip('%'))*0.05)
+		if rating_node:
+			return round(float(rating_node[0].strip('%').strip())*0.05)
+	
+	def parse_covers(self, root):
+		from calibre_plugins.moly_hu import Moly_hu
+		book_covers = root.xpath('(//*[@class="coverbox"]//a/@href)[position()<=%d]'%self.max_covers)
+		if book_covers:
+			return [Moly_hu.BASE_URL + cover_url for cover_url in book_covers]
+	
+	def parse_languages(self, tags):
+		langs = []
+		for tag in tags:
+			langId = self._translateLanguageToCode(tag)
+			if langId is not None:
+				langs.append(langId)
+		
+		if not langs:
+			return ['hu']
+		
+		return langs
+	
+	def _translateLanguageToCode(self, displayLang):
+		displayLang = displayLang.lower().strip() if displayLang else None
+		langTbl = { None: 'und', 
+					u'angol nyelv\u0171': 'en',
+					u'n\xe9met nyelv\u0171': 'de',
+					u'francia nyelv\u0171': 'fr',
+					u'olasz nyelv\u0171': 'it', 
+					u'spanyol nyelv\u0171': 'es',
+					u'orosz nyelv\u0171': 'ru',
+					u't\xf6r\xf6k nyelv\u0171': 'tr',
+					u'g\xf6r\xf6g nyelv\u0171': 'gr',
+					u'k\xednai nyelv\u0171': 'cn',
+					u'jap\xe1n nyelv\u0171': 'jp' }
+		return langTbl.get(displayLang, None)
